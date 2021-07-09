658f0b9e899faab1fe04429809bdd26b1b0a6a92
davinerd <anathema@anche.no>
2021-07-09 12:05:04 +0200

    Added network resource to output

--- output.tf
+++ output.tf
@@ -9,3 +9,9 @@ output "get_credentials" {
   description = "Gcloud get-credentials command"
   value       = format("gcloud container clusters get-credentials --project %s --region %s %s", var.project_id, var.region, var.name)
 }
+
+
+output "network" {
+  description = "Network details"
+  value = google_compute_network.network
+}
